9157c82123dac5f65e01613c0b1c4d523ff100e4
easylogic <cyberuls@gmail.com>
2017-07-04 12:25:06 +0900

    add fonteditor-core module

---
+++ src/fonteditor/widget/glyf-unicode-name.js
@@ -0,0 +1,16 @@
+/**
+ * @file 글리프 유니코드 이름 타입 
+ * @author mengke01(kekee000@gmail.com)
+ */
+
+
+define(
+    function (require) {
+
+        var constraint = {
+            'KSC5601': 'KSC5601'
+		};
+
+        return constraint;
+    }
+);
