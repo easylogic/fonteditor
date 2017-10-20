ecc52f9f1fb32181cdf0fcfc8f8d9529a65bf30b
easylogic <cyberuls@gmail.com>
2017-10-20 19:07:30 +0900

    modify i18n code

--- build/i18n.zh-cn.js
+++ build/i18n.zh-cn.js
@@ -10,7 +10,7 @@ module.exports = exports = {
     tabs_glyf: "Glyf",
     tabs_simple: "Text Writer",    
     tabs_subsetting: "Subset",       
-    tabs_Editor: "Editor",             
+    tabs_editor: "Editor",         
     newglyph: '新字形',
     undo: '撤销',
     redo: '重做',
